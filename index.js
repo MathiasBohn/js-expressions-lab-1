4199a97e58db2b6943bfee2e98d6462b4c84dac7
Mathias <mathiasbohndk@gmail.com>
2025-08-06 18:13:37 -0400

    Passing Tests

--- index.js
+++ index.js
@@ -14,7 +14,32 @@
 //! After creating the four variables mentioned above, uncomment the following lines
 //* This way you can export them to the test file, this is essential for the tests to work
 
-module.exports = {
+
+
+// Temperature Data
+    const celsiusTemps = [25, 18, 15, 28, 20, 23, 30, 22, 24, 21, 27, 19, 26, 17, 29];
+    const fahrenheitTemps = [32, 70, 80, 72, 68, 75, 82, 65, 77, 78, 73, 79, 71, 74, 76];
+
+    // Data Conversions
+    const celsiusTempsToFahrenheit = celsiusTemps.map(temp => ((temp * 9 / 5) + 32));
+    const fahrenheitTempsToCelsius = fahrenheitTemps.map(temp => ((temp - 32) * 5 / 9));
+
+    // Data in full list
+    const allTempsInFahrenheit = fahrenheitTemps.concat(celsiusTempsToFahrenheit)
+    const allTempsInCelsius = celsiusTemps.concat(fahrenheitTempsToCelsius)
+
+    // Calculate the total temperatures
+    const tot_temperature_in_fahrenheit = allTempsInFahrenheit.reduce((sum, temp) => sum + temp, 0);
+    const tot_temperature_in_celsius = allTempsInCelsius.reduce((sum, temp) => sum + temp, 0);
+    
+    // Calculate mean of temperatures
+    const avg_temperature_in_celsius = tot_temperature_in_celsius / allTempsInCelsius.length;
+    const avg_temperature_in_fahrenheit = tot_temperature_in_fahrenheit / allTempsInFahrenheit.length;
+
+    console.log(avg_temperature_in_celsius)
+    console.log(avg_temperature_in_fahrenheit)
+
+    module.exports = {
     // tot_temperature_in_fahrenheit,
     // tot_temperature_in_celsius,
     // avg_temperature_in_fahrenheit,
